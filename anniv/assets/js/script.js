bf309120da345829ec3f5d8fff1e1b44ed26a4f1
HanZW02 <165407608+HanZW02@users.noreply.github.com>
2024-08-03 21:02:44 +0700

    Add files via upload

---
+++ anniv/assets/js/script.js
@@ -0,0 +1,28 @@
+AOS.init()
+
+lightbox.option({
+	'alwaysShowNavOnTouchDevices': true,
+	'wrapAround': true
+})
+
+var musik = ''
+var audio = document.querySelector('.audio')
+if (musik) 
+{
+	audio.src = musik
+}
+
+function mulai() 
+{
+	window.scrollTo(0, 0)
+	var mail_section = $('#mail-section')
+	$('#mail').attr('src', 'assets/img/mail.gif')
+	setTimeout(function() {
+		mail_section.css('opacity', 0)
+		$('body').removeClass('overflow-hidden')
+	}, 2000)
+	setTimeout(function() {
+		mail_section.removeClass('d-flex')
+		mail_section.addClass('d-none')
+	}, 4000)
+}
